1284540076bbcc7bcfecb8a0e8157bd8a2e7f827
lixianliang <lixianliang@corp.netease.com>
2015-11-02 14:06:38 +0800

    fix bug: 源代码没有指定全路径

--- wechat.sh
+++ wechat.sh
@@ -1,5 +1,5 @@
-clang -dynamiclib main.m -fobjc-link-runtime -current_version 1.0 -compatibility_version 1.0 -o wechat_no_revoke.A.dylib
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+clang -dynamiclib ${DIR}/main.m -fobjc-link-runtime -current_version 1.0 -compatibility_version 1.0 -o wechat_no_revoke.A.dylib
 if [ "$?" -eq "0" ]; then
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   DYLD_INSERT_LIBRARIES=${DIR}/wechat_no_revoke.A.dylib /Applications/WeChat.app/Contents/MacOS/WeChat
 fi
